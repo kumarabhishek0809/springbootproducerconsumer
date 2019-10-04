eaec6e6f5dc7f64d068331cf62426b8b37ebaf20
Kumar.Abhishek <Kumar.abhishek@emaratech.ae>
2019-10-04 15:16:19 +0400

    added new project

---
+++ src/main/java/com/springbootproducerconsumer/SpringbootproducerconsumerApplication.java
@@ -0,0 +1,13 @@
+package com.springbootproducerconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootproducerconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootproducerconsumerApplication.class, args);
+	}
+
+}
